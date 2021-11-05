998d10a51dd75b305bf51aa8557d8c5ce19df0a3
koyaabe <koyatendo@gmail.com>
2021-11-05 17:29:59 +0900

    Initial Commit

---
+++ LoginFunctionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginFunctionApp
+//
+//  Created by Koya on 2021/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
