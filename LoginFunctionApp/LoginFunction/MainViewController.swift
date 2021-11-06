6d64e2bb460ee30c1d7a203ebd44fa462f832fbe
koyaabe <koyatendo@gmail.com>
2021-11-06 16:17:35 +0900

    Add logout function

--- LoginFunctionApp/LoginFunction/MainViewController.swift
+++ LoginFunctionApp/LoginFunction/MainViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet weak var loggedInUserName: UILabel!
+    @IBOutlet weak var loggedInPassword: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        // 試しに名前とパスワードを表示してみる
+        let userName = UserDefaults.standard.string(forKey: "userName")
+        loggedInUserName.text = userName
+        
+        let password = UserDefaults.standard.string(forKey: "password")
+        loggedInPassword.text = password
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -23,6 +31,13 @@ class MainViewController: UIViewController {
             let loginVC = storyboard?.instantiateViewController(withIdentifier: "LoginView")
             self.present(loginVC!, animated: true, completion: nil)
         }
+        
+    }
+    
+    @IBAction func logoutButton(_ sender: Any) {
+        
+        UserDefaults.standard.set(false, forKey: "LoggedIn")
+        self.dismiss(animated: true, completion: nil)
         
     }
     
